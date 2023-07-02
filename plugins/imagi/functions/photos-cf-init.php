bf8026eb0c77f3032f73e7450baa5336ee0388e1
gio <gio>
2023-07-02 20:04:44 +0800

    photos_save_post not yet complete

--- plugins/imagi/functions/photos-cf-init.php
+++ plugins/imagi/functions/photos-cf-init.php
@@ -16,24 +16,56 @@ function photos_cf_init()
 
 function gallery_content()
 {
+
+    // pre-populate fields with data if exists
+
+    /* global $wpdb; */
+    /* $table = $wpdb->prefix . "imagi_gallery"; */
+    /* $work_title = $wpdb->get_var("SELECT `title` FROM $table"); */
+
 ?>
 
-    <form action="">
-
+    <form method="post">
         <label for="work_title">Work title:</label>
         <input type="text" name="work_title">
-
     </form>
 
 <?php
 }
 
-/* function save_photo()
+function photos_save_post($post_id)
 {
-        $work_title = $_POST['work_title'];
 
-        global $wpdb;
+    $work_title = $_POST['work_title'];
+    $id = get_the_id();
 
-        $wpdb->$prefix.
+    global $wpdb;
+    $wpdb->insert(
+        $wpdb->prefix . "imagi_gallery",
+        [
+            'photo_ID' => $id,
+            'title' => $work_title
+        ]
+    );
 }
- */
+
+add_action('save_post', 'photos_save_post');
+
+
+// include('photos-save-post.php');
+
+
+    /* $photo_ID = get_the_id();
+    $photo_title = get_the_title();
+    $work_title = $_POST['work_title'];
+
+    global $wpdb;
+    $wpdb->update(
+        $wpdb->prefix . "imagi_gallery",
+        [
+            'title' => $work_title
+        ],
+        [
+            'photo_ID' => $post_id;
+        ]
+    ); */
---
+++ plugins/imagi/functions/photos-save-post.php
@@ -0,0 +1,10 @@
+<?php
+
+/* function photos_save_post($post_id)
+{
+    $work_title = $_POST('work_title');
+
+}
+
+add_action('save_post', 'photos_save_post');
+ */
