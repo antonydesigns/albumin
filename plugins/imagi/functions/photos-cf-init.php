2979d3f2d51dc4b14f2d83f0842ab6cc7286b7ba
gio <gio>
2023-07-05 17:58:19 +0800

    add insert and update functionality

--- plugins/imagi/functions/photos-cf-init.php
+++ plugins/imagi/functions/photos-cf-init.php
@@ -6,7 +6,7 @@ function photos_cf_init()
     add_meta_box(
         'photos_cf',
         'Gallery Content',  // Custom field title      
-        'gallery_content',  // callback name
+        'custom_field_content',  // callback name
         'photos',           // custom post type name
         'normal',
         'low'
@@ -14,33 +14,36 @@ function photos_cf_init()
 }
 
 
-function gallery_content()
+function custom_field_content()
 {
 
     // pre-populate fields with data if exists
 
-    /* global $wpdb; */
-    /* $table = $wpdb->prefix . "imagi_gallery"; */
-    /* $work_title = $wpdb->get_var("SELECT `title` FROM $table"); */
+    global $wpdb;
+    $table = $wpdb->prefix . "imagi_gallery";
+    $id = get_the_id();
+    $work = $wpdb->get_var("SELECT `title` FROM $table WHERE `photo_id` = $id");
+    echo $work;
+    echo "<br/>";
 
 ?>
 
     <form method="post">
         <label for="work_title">Work title:</label>
-        <input type="text" name="work_title">
+        <input type="text" name="work_title" value="<?php echo $work ?>">
     </form>
 
 <?php
 }
 
-function photos_save_post($post_id)
+function photos_save_post()
 {
-
+    if (!isset($_POST['work_title'])) return;
     $work_title = $_POST['work_title'];
     $id = get_the_id();
 
     global $wpdb;
-    $wpdb->insert(
+    $wpdb->insert(                          // ADD OR INSERT NEW DATA
         $wpdb->prefix . "imagi_gallery",
         [
             'photo_ID' => $id,
@@ -49,23 +52,23 @@ function photos_save_post($post_id)
     );
 }
 
-add_action('save_post', 'photos_save_post');
-
-
-// include('photos-save-post.php');
-
-
-    /* $photo_ID = get_the_id();
-    $photo_title = get_the_title();
+function photos_update_post()
+{
+    if (!isset($_POST['work_title'])) return;
     $work_title = $_POST['work_title'];
+    $id = get_the_id();
 
     global $wpdb;
-    $wpdb->update(
+    $wpdb->update(                          // UPDATE EXISTING DATA
         $wpdb->prefix . "imagi_gallery",
         [
             'title' => $work_title
         ],
         [
-            'photo_ID' => $post_id;
+            'photo_ID' => $id
         ]
-    ); */
+    );
+}
+
+add_action('save_post', 'photos_save_post');
+add_action('save_post', 'photos_update_post');
